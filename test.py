0f8e257896f107896635fb7a8066c64e0edcfe92
JOHNPAUL ADIMONYEMMA <johnpauladimonyemma@gmail.com>
2021-04-17 14:39:48 +0100

    updated

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from Budget import Budget
-from time import time
+
 
 
 '''
@@ -21,6 +21,11 @@ clothing.deposit(10000, 'Christmas cloth')
 clothing.withdraw(30000, 'for food')
 
 
+entertainment.deposit(10500, 'Christmas cloth')
+entertainment.withdraw(3000, 'for food')
+
+
 
 print(f'You total balance is  {food.getDetails()} for  food category')
 print(f'You total balance is  {clothing.getDetails()} for  clothing category')
+print(f'You total balance is  {entertainment.getDetails()} for  entertainment category')
